8223fd40106c414dfd68c93b8372895973615ad1
Sven Joseph <sven@splynter.eu>
2022-05-30 14:15:25 +0200

    Added initial http-client for API

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,22 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    type: string;
+    material: string;
+    color: string;
+    size: number;
+    finishing: string;
+    finishMaterial1: string;
+    finishMaterial2: string;
+    finishMaterial3: string;
+    hook1: string;
+    hook2: string;
+    oRing1: string;
+    oRing2: string;
+    stopBar: string;
+    keychain: string;
+    endCaps: string;
+    price: number;
+    inStock: number;
+    pictureUrl: string;
+}
